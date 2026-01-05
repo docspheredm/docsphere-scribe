51897ba7c54283ceb838eceea9efb6ef43ee776f
docspheredm <docspheredm@gmail.com>
2026-01-05 18:39:38 +0530

    Create global.d.ts

---
+++ global.d.ts
@@ -0,0 +1,23 @@
+declare module '@google/genai' {
+  export class GoogleGenerativeAI {
+    constructor(apiKey: string);
+    getGenerativeModel(config: { model: string }): any;
+  }
+}
+
+declare module 'jspdf' {
+  export class jsPDF {
+    constructor(options?: any);
+    text(text: string, x: number, y: number): void;
+    save(filename: string): void;
+    addPage(): void;
+    setFontSize(size: number): void;
+    setFont(font: string): void;
+    internal: {
+      pageSize: {
+        height: number;
+        width: number;
+      };
+    };
+  }
+}
